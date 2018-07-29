83baef85042606bc2323d4c0317c114d80303a4a
TripleBlackCat <kavilann@gmail.com>
2018-07-29 14:56:22 +0200

    data aug and data read in

--- ML/cnn.py
+++ ML/cnn.py
@@ -11,16 +11,20 @@ from keras.layers import Dense, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras.models import Sequential
 from keras.preprocessing.image import load_img
+from sklearn.model_selection import train_test_split
+from keras.preprocessing.image import ImageDataGenerator
 from keras.preprocessing import image
 import matplotlib.pylab as plt
-
+import random
+from sklearn.preprocessing import LabelBinarizer
+from keras.preprocessing.image import img_to_array
 import numpy as np
 import cv2
 from numpy import array
 from keras import backend as K
 K.tensorflow_backend._get_available_gpus()
-
-batch_size = 3
+from imutils import paths
+batch_size = 1
 num_classes = 2
 epochs = 10
 
@@ -30,54 +34,45 @@ img_x, img_y = 1092, 315
 
 # load the MNIST data set, which already splits into train and test sets for us
  
-path = os.getcwd() + '/Train/'
-fileNames = os.listdir(path)
+path = os.getcwd() + '/dataset/'
+folderNames = sorted(list(os.listdir(path)))
+random.seed(42)
+random.shuffle(folderNames)
+print(folderNames[0])
 
-y_train = []
-x_train = []
-y_test = []
-x_test = []
+# y_train = []
+# x_train = []
+# y_test = []
+# x_test = []
 
-for fileName in fileNames:
-    w, x, _, _, = fileName.split('-')
-    name = w + " " + x
-    print(name)
-    if name == "Andropadus importunus":
-        y_train.append(0)
-    else:
-        y_train.append(1)
-    
-    im = cv2.imread(path + fileName)
-    x_train.append(im)
+data = []
+labels = []
+
+for folderName in folderNames:
+    folderPath = path + folderName
+    fileNames = sorted(list(os.listdir(folderPath)))
+    for imageName in fileNames:
+        fullPath = folderPath + '/' + imageName
+        print(fullPath)
+        image = cv2.imread(fullPath)
+        image = cv2.resize(image, (img_x, img_y))
+        image = img_to_array(image)
+        data.append(image)
+        label = folderName.split(os.path.sep)
+        print(label)
+        labels.append(label)
 
 
-print("Number of train variables: " + str(len(x_train)))
-print("Number of train labels: " + str(len(y_train)))
+data = np.array(data, dtype="float") /255.0
+labels = np.array(labels)
+print("[INFO] data matrix: {:.2f}MB".format(
+	data.nbytes / (1024 * 1000.0)))
 
-path = os.getcwd() + '/Test/'
-fileNames = os.listdir(path)
+lb = LabelBinarizer()
+labels = lb.fit_transform(labels)
 
-for fileName in fileNames:
-    w, x, _, _ = fileName.split('-')
-    name = w + " " + x
-    im_test = cv2.imread(path + fileName)
-    x_test.append(im_test)
-    #y_test.append(name)
-    if name == "Telophorus zeylonus":
-        y_test.append(0)
-    else:
-        y_test.append(1)
-    
-print("Number of test variables: " + str(len(x_test)))
-print("Number of test labels: " + str(len(y_test)))
 
-# # reshape the data into a 4D tensor - (sample_number, x_img_size, y_img_size, num_channels)
-# # because the MNIST is greyscale, we only have a single channel - RGB colour images would have 3
-x_train = array( x_train )
-x_test = array( x_test )
-
-x_train = x_train.reshape(x_train.shape[0], img_x, img_y, 3)
-x_test = x_test.reshape(x_test.shape[0], img_x, img_y, 3)
+(x_train, x_test, y_train, y_test) = train_test_split(data, labels, test_size=0.2, random_state=42)
 
 print("x train set shape")
 print(x_train.shape)
@@ -88,25 +83,18 @@ input_shape = (img_x, img_y, 3)
 print("Input shape:")
 print(input_shape)
 
-
-# convert the data to the right type
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
-x_train /= 255   #due to pixel being able to take 1-255 values, normalization
-x_test /= 255
-print('x_train shape:', x_train.shape)
-print(x_train.shape[0], 'train samples')
-print(x_test.shape[0], 'test samples')
+aug = ImageDataGenerator(rotation_range=25, width_shift_range=0.1,
+	height_shift_range=0.1, shear_range=0.2, zoom_range=0.2,
+	horizontal_flip=True, fill_mode="nearest")
 
 
+# # convert class vectors to binary class matrices - this is for use in the categorical_crossentropy loss below
+# y_train = keras.utils.to_categorical(y_train, 2) 
+# y_test = keras.utils.to_categorical(y_test, 2)
 
-# convert class vectors to binary class matrices - this is for use in the categorical_crossentropy loss below
-y_train = keras.utils.to_categorical(y_train, 2) 
-y_test = keras.utils.to_categorical(y_test, 2)
-
-# need one hot encoding
-print(type(y_train)) #(107,2)
-print(y_test) #(0,2) 
+# # need one hot encoding
+# print(type(y_train)) #(107,2)
+# print(y_test) #(0,2) 
 
 model = Sequential()
 model.add(Conv2D(32, kernel_size=(5, 5), strides=(5, 5), activation='relu', input_shape=input_shape))
