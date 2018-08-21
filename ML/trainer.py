de2c36509865b0db82e8831393e80747d86ddd26
Vorapahct <iordtchap@gmail.com>
2018-08-21 23:38:24 +0200

    Refactored Training Code

--- ML/trainer.py
+++ ML/trainer.py
@@ -1,165 +1,157 @@
-import os
-import random
-import keras
-from keras.optimizers import Adam
-from keras.datasets import mnist
+from keras import backend as K
 from keras.layers import Dense, Flatten
 from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, BatchNormalization
 from keras.models import Sequential
-from keras.preprocessing.image import load_img
-from sklearn.model_selection import train_test_split
+from keras.callbacks import ModelCheckpoint
+from keras.optimizers import Adam
 from keras.preprocessing.image import ImageDataGenerator
-from keras.preprocessing import image
-import matplotlib.pylab as plt
-import random
-from sklearn.preprocessing import LabelBinarizer
 from keras.preprocessing.image import img_to_array
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+import os
+import random
 import numpy as np
 import cv2
-from numpy import array
+import glob
 import pickle
 import matplotlib.pyplot as plt
-from imutils import paths
-import tensorflow as tf
-from keras import backend as K
-from keras.models import Sequential
-from keras.layers.normalization import BatchNormalization
-from keras.layers.convolutional import Conv2D
-from keras.layers.convolutional import MaxPooling2D
-from keras.layers.core import Activation
-from keras.layers.core import Flatten
-from keras.layers.core import Dropout
-from keras.layers.core import Dense
-
 
 # Load Data from folder 
-dataFolder = '/Cleaned_Data/'
-path = os.getcwd() + dataFolder
-folderNames = sorted(list(os.listdir(path)))
-random.seed(42)
-random.shuffle(folderNames)
+current_path = os.getcwd()
+All_files = glob.glob(current_path + '/Reduced_Data_further/**/*')
+print("Number of total files: " + str(len(All_files)))
 
-########--PARAMS--#########
-EPOCHS = 5                #
-INIT_LR = 1e-3            #
-BS = 5                    #
-img_x, img_y = 1092, 315  #
-IMAGE_DIMS = (200, 100, 3)#
-modelName = 'it_9.model'  #
-###########################
+######--HYPERPARAMETERS--#######
+Epochs = 1             
+Learning_Rate = 1e-5            
+Batch_Size = 20                    
+Image_Dimensions = (120, 80, 3)
+model_name = '1-Final'  
+################################
 
-# Load data and labels into memory
+# data and label intitialization
+os.makedirs(model_name)
 data = []
 labels = []
 
-for folderName in folderNames:
-    folderPath = path + folderName
-    fileNames = sorted(list(os.listdir(folderPath)))
-    for imageName in fileNames:
-        fullPath = folderPath + '/' + imageName
-        image = cv2.imread(fullPath)
-        image = cv2.resize(image, (IMAGE_DIMS[0], IMAGE_DIMS[1]))
-        image = img_to_array(image)
-        data.append(image)
-        label = folderName.split(os.path.sep)
-        labels.append(label)
+random.shuffle(All_files)
+
+for image_name in All_files:
+    image_data = cv2.imread(image_name)
+    image_data = cv2.resize(image_data, (Image_Dimensions[0], Image_Dimensions[1]))
+    image_data = img_to_array(image_data)
+    data.append(image_data)
+    label = image_name.split(os.path.sep)
+    labels.append(label[-2])
 
 data = np.array(data, dtype="float") /255.0
 labels = np.array(labels)
 
-print("[INFO] data matrix: {:.1f}MB".format(
-	data.nbytes / (1024 * 1000.0)))
-
 lb = LabelBinarizer()
 labels = lb.fit_transform(labels)
 
-# Split Data
-(x_train, x_test, y_train, y_test) = train_test_split(data, labels, test_size=0.2, random_state=42)
+# Data split into training and testing set
+(x_train, x_test, y_train, y_test) = train_test_split(data, labels, test_size=0.2)
 
 # Data Augmentation
-aug = ImageDataGenerator(rotation_range=25, width_shift_range=0.1,
-        height_shift_range=0.1, shear_range=0.2, zoom_range=0.2,
-        horizontal_flip=True, fill_mode="nearest")
+data_augmentation = ImageDataGenerator(width_shift_range=0.15, height_shift_range=0.15, shear_range=0.25, zoom_range=0.2)
 
-# Setup Model
-model = Sequential()
-width = IMAGE_DIMS[0]
-height = IMAGE_DIMS[1]
-depth = IMAGE_DIMS[2]
-inputShape = (height, width, depth)
-chanDim = -1
+# Model inputs
+width = Image_Dimensions[0]
+height = Image_Dimensions[1]
+depth = Image_Dimensions[2]
+input_parameters = (height, width, depth)
+channel_dimensions = -1
 classes = len(lb.classes_)
 
+# Channels depend on the OS used
 if K.image_data_format() == "channels_first":
-    inputShape = (depth, height, width)
-    chanDim = 1
+    input_parameters = (depth, height, width)
+    channel_dimensions = 1
 
-#################################################################################
-model.add(Conv2D(64, (5, 5), padding="same", input_shape=inputShape))        
+# Defining model architecture
+model = Sequential()
+model.add(Conv2D(32, (3, 3), padding="same", input_shape=input_parameters))
 model.add(Activation("relu"))
-model.add(BatchNormalization(axis=chanDim))
-model.add(MaxPooling2D(pool_size=(3, 3)))
-model.add(Dropout(0.25))
-model.add(Conv2D(128, (5, 5), padding="same"))
-model.add(Activation("relu"))
-model.add(BatchNormalization(axis=chanDim))
-model.add(Conv2D(128, (5, 5), padding="same"))
-model.add(Activation("relu"))
-model.add(BatchNormalization(axis=chanDim))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-model.add(Conv2D(256, (5, 5), padding="same"))
-model.add(Activation("relu"))
-model.add(BatchNormalization(axis=chanDim))
-model.add(Conv2D(256, (5, 5), padding="same"))
-model.add(Activation("relu"))
-model.add(BatchNormalization(axis=chanDim))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-model.add(Flatten())
-model.add(Dense(1024))
-model.add(Activation("relu"))
-model.add(BatchNormalization())
+model.add(BatchNormalization(axis=channel_dimensions))
 model.add(Dropout(0.5))
+
+model.add(Conv2D(32, (3, 3), padding="same"))
+model.add(Activation("relu"))
+model.add(BatchNormalization(axis=channel_dimensions))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.5))
+
+model.add(Conv2D(64, (3, 3), padding="same"))
+model.add(Activation("relu"))
+model.add(BatchNormalization(axis=channel_dimensions))
+model.add(Dropout(0.5))
+
+model.add(Conv2D(64, (3, 3), padding="same"))
+model.add(Activation("relu"))
+model.add(BatchNormalization(axis=channel_dimensions))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.5))
+
+model.add(Conv2D(128, (3, 3), padding="same"))
+model.add(Activation("relu"))
+model.add(BatchNormalization(axis=channel_dimensions))
+model.add(Dropout(0.5))
+
+model.add(Conv2D(128, (3, 3), padding="same"))
+model.add(Activation("relu"))
+model.add(BatchNormalization(axis=channel_dimensions))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.5))
+
+model.add(Flatten())
+model.add(Dense(1024, use_bias=False))
+model.add(BatchNormalization())
+model.add(Activation("relu"))
+model.add(Dropout(0.8))
+
 model.add(Dense(classes))
 model.add(Activation("softmax"))
-print(model.summary())
-################################################################################
 
-# Optimizer
-opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
-model.compile(loss="categorical_crossentropy", optimizer=opt,
-metrics=["accuracy"])
+# Model compilation
+opt = Adam(lr=Learning_Rate, decay=Learning_Rate/Epochs)
+model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
+filepath= model_name + "/weights-improvement-{epoch:02d}-{val_acc:.2f}.hdf5"
+checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
+callbacks_list = [checkpoint]
 
 # Train Network
-print("[INFO] training network...")
-H = model.fit_generator(
-    aug.flow(x_train, y_train, batch_size=BS),
-    validation_data=(x_test, y_test),
-    steps_per_epoch=len(x_train) // BS,
-    epochs=EPOCHS, verbose=1)
-
+model_history = model.fit_generator(data_augmentation.flow(x_train, y_train, batch_size=Batch_Size), validation_data=(x_test, y_test),
+   steps_per_epoch=(len(x_train)//Batch_Size), epochs=Epochs, verbose=1, callbacks=callbacks_list)
+data_augmentation.flow(x_train, y_train, batch_size=Batch_Size)
 
 # Save Model 
-print("[INFO] saving model...")
-model.save(modelName)
+model.save(model_name+'.model')
 
 # Save Labels
-print("[INFO] serializing label binarizer...")
-f = open('lb.pickle', "wb")
-f.write(pickle.dumps(lb))
-f.close()
+pickle_file = open('lb.pickle', "wb")
+pickle_file.write(pickle.dumps(lb))
+pickle_file.close()
 
-# Plot the training loss and accuracy
-plt.style.use("ggplot")
+# Plot loss and accuracy of model
+plt.style.use("bmh")
+N = Epochs
+
 plt.figure()
-N = EPOCHS
-# plt.plot(np.arange(0, N), H.history["loss"], label="train_loss")
-# plt.plot(np.arange(0, N), H.history["val_loss"], label="val_loss")
-plt.plot(np.arange(0, N), H.history["acc"], label="train_acc")
-plt.plot(np.arange(0, N), H.history["val_acc"], label="val_acc")
-plt.title("Training Loss and Accuracy")
-plt.xlabel("Epoch #")
-plt.ylabel("Loss/Accuracy")
+plt.plot(np.arange(0, N), model_history.history["acc"], label="Training Accuracy")
+plt.plot(np.arange(0, N), model_history.history["val_acc"], label="Validation Accuracy")
+plt.title("Accuracy vs Validation Accuracy")
+plt.xlabel("Epoch Number")
+plt.ylabel("Accuracy")
 plt.legend(loc="upper left")
-plt.savefig(modelName+'.png')
+plt.savefig(model_name + '-acc.png')
+
+plt.figure()
+plt.plot(np.arange(0, N), model_history.history["loss"], label="Training Loss")
+plt.plot(np.arange(0, N), model_history.history["val_loss"], label="Validation Loss")
+plt.title("Loss vs Validation Loss")
+plt.xlabel("Epoch Number")
+plt.ylabel("Loss")
+plt.legend(loc="upper left")
+plt.savefig(model_name + '-loss.png')
